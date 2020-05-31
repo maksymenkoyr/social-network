b80b9167c64d3813383db10c1adddc2effda0a18
CodeJesus24 <maksimenko.yr@gmail.com>
2020-05-31 16:22:38 +0200

    add the message page

--- src/components/AppContent/Messenges/Messages.js
+++ src/components/AppContent/Messenges/Messages.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import "./Messages.scss"
 
 const Messages = () => {
     return (
-        <main className="app-content messages">
-            <div className></div>
+        <main className='app__content messages'>
+            <ul className='conversation'>
+                <li className='conversation__message'>HI</li>
+                <li className='conversation__message'>By</li>
+                <li className='conversation__message'>You</li>
+                <li className='conversation__message'>are</li>
+                <li className='conversation__message'>mine</li>
+            </ul>
+            <ul className='conversations-list'>
+                <li className='conversations-list__item'>Tom</li>
+                <li className='conversations-list__item'>Bom</li>
+                <li className='conversations-list__item'>Jom</li>
+            </ul>
         </main>
-                      
-        
     )
 }
 
