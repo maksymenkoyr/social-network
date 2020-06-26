dc06705d321c67e4be3e2ecabcf732d4ce98314c
CodeJesus24 <maksimenko.yr@gmail.com>
2020-06-26 22:06:05 +0200

    change formik to react-hook-form

--- src/components/SignIn/SignInForm.js
+++ src/components/SignIn/SignInForm.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import {Input} from '../common/CostumFormComponents'
-import {useFormik} from 'formik'
+import {useForm} from 'react-hook-form'
 
 const SignInForm = () => {
-    const signIn = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-            remberMe: false,
-        },
-        onSubmit: values => {
-            console.log(values)
-        },
-    })
+    const {register, handleSubmit, errors} = useForm()
+    const onSubmit = data => console.log(data)
+
     return (
-        <form className='sign-in-page__form'>
-            <input placeholder='login' name='login' type='text' />
-            <input placeholder='password' name='password' type='password' />
-            <input name='remberMe' type='checkbox' />
-            <button type='submit'>sign in</button>
+        <form className='sign-in-page__form' onSubmit={handleSubmit(onSubmit)}>
+            <input placeholder='login' name='login' type='text' ref={register({required: true})} />
+            {errors.login && <p>error</p>}
+            <input placeholder='password' name='password' type='password' ref={register} />
+            <input name='remberMe' type='checkbox' ref={register} />
+
+            <button type='submit' ref={register}>
+                sign in
+            </button>
         </form>
     )
 }
