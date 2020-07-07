1cdf9095b119a6125d01b68537dd0fb6b4861571
CodeJesus24 <maksimenko.yr@gmail.com>
2020-07-07 19:16:27 +0200

    ui: HorizontalLoader: create

---
+++ src/ui/preloaders/HorizontalLoader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HorizontalLoader.scss'
+
+const HorizontalLoader = () => {
+    return (
+        <div className='horizontal-loader'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default HorizontalLoader
