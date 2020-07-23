3f17f0f72eb3f35739df5ccef14da7346337084c
CodeJesus24 <maksimenko.yr@gmail.com>
2020-07-23 17:33:39 +0200

    ui: create cirkle loader

---
+++ src/ui/loaders/CirkleLoader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './CirkleLoader.scss'
+
+const CirkleLoader = () => {
+    return (
+        <div className='cirkle-loader'>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default CirkleLoader
