7a60332b2d33d21892580a5ae79594204296a055
CodeJesus24 <maksimenko.yr@gmail.com>
2020-08-22 15:48:34 +0200

    add prewieving of uploading profile photo

--- src/features/profile/EditProfilePhoto.js
+++ src/features/profile/EditProfilePhoto.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 
 const EditProfilePhoto = props => {
     const [photo, setPhoto] = useState()
+    const [imgSrc, setImgSrc] = useState()
+
     return (
         <div className='edit-photo-modal'>
             <div className='edit-photo-modal__content'>
@@ -10,12 +12,15 @@ const EditProfilePhoto = props => {
                     type='file'
                     onChange={event => {
                         setPhoto(event.target.files[0])
+                        const reader = new FileReader()
+                        reader.readAsDataURL(event.target.files[0])
+                        reader.onload = () => {
+                            setImgSrc(reader.result)
+                        }
+                        setImgSrc(reader.result)
                     }}
                 ></input>
-                <img
-                    className='edit-photo-modal__img'
-                    // src={window.URL.createObjectURL(photo)}
-                ></img>
+                <img className='edit-photo-modal__img' src={imgSrc}></img>
                 <button
                     onClick={() => {
                         const formData = new FormData()
