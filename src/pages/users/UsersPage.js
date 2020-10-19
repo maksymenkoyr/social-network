ac86047f57ed2ff577b9821923769aa5c3075d39
CodeJesus24 <maksimenko.yr@gmail.com>
2020-10-19 15:33:02 +0200

    users page fix

--- src/pages/users/UsersPage.js
+++ src/pages/users/UsersPage.js
@@ -2,12 +2,15 @@ import React from 'react'
 import {Initialization} from '../../features/initialization'
 import MainPageTemplate from '../templates/MainPageTemplate'
 import {Users} from '../../features/users'
+import './UsersPage.scss'
 
 const UsersPage = () => {
     return (
         <Initialization>
             <MainPageTemplate>
-                <Users />
+                <div className='users-page'>
+                    <Users />
+                </div>
             </MainPageTemplate>
         </Initialization>
     )
